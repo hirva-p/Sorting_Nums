d214a58144fe8404e4dc247a3b1328303e127947
hirva-p <140225907+hirva-p@users.noreply.github.com>
2023-08-17 22:31:07 +0530

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -3,7 +3,7 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 import numpy as np
 import time
-# from streamlit_autorefresh import st_autorefresh
+import st_autorefresh
 
 
 st.title("Sorting Numbers Simulation")
